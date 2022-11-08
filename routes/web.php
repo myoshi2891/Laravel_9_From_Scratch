461b9f31784e51ecfd37f30f06441cd5bd53c610
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-08 23:34:55 +0900

    Use Caching for Expensive Operations

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
-        // abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
 
-    return view('post', [
-        'post' => $post
-    ]);
-});
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+');
