6f761b0b54f4d02cc34f5a2fd2a7741a207bcfd3
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-09 22:59:32 +0900

    Find a Composer Package for Post Metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,28 +2,58 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post
 {
+  public $title;
+
+  public $excerpt;
+
+  public $date;
+
+  public $body;
+
+  public $slug;
+
+  public function __construct($title, $excerpt, $date, $body, $slug)
+  {
+    $this->title = $title;
+    $this->excerpt = $excerpt;
+    $this->date = $date;
+    $this->body = $body;
+    $this->slug = $slug;
+  }
+
   public static function all()
   {
-    $files = File::files(resource_path("posts/"));
-
-    return array_map(
-      fn ($file) =>
-      $file->getContents(),
-      $files
-    );
+    return collect(File::files(resource_path("posts")))
+      ->map(
+        fn ($file) =>
+        YamlFrontMatter::parseFile($file)
+      )
+      ->map(
+        fn ($document) =>
+        new Post(
+          $document->title,
+          $document->excerpt,
+          $document->date,
+          $document->body(),
+          $document->slug
+        )
+      );
   }
 
   public static function find($slug)
   {
-    if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-      throw new ModelNotFoundException();
-    }
+    return static::all()->firstWhere('slug', $slug);
+    // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+    //   throw new ModelNotFoundException();
+    // }
 
-    return cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
+    // return cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    // $posts = array_map(function ($file) {
+    //     $document = YamlFrontMatter::parseFile($file);
+
+    //     return $posts[] = new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body(),
+    //         $document->slug
+    //     );
+    // }, $files);
+
+    // foreach ($files as $file) {
+    //     $document = YamlFrontMatter::parseFile($file);
+    //     $posts[] = new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body(),
+    //         $document->slug,
+    //     );
+    // }
+
     return view('posts', [
         'posts' => Post::all()
     ]);
