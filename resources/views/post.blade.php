e081fd720955f55e004c13d58349f58b386d49af
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-12 00:45:20 +0900

    Make a Post Model and Migration

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
 
@@ -9,4 +7,4 @@
         </div>
     </article>
     <a href="/">Go Back</a>
-@endsection
+</x-layout>
