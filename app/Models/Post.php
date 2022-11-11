50362333d8d2f2cff642cedd3e0940529bb9941e
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-11 23:37:19 +0900

    A Few Tweaks and Consideration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -53,10 +53,16 @@ class Post
   public static function find($slug)
   {
     return static::all()->firstWhere('slug', $slug);
-    // if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-    //   throw new ModelNotFoundException();
-    // }
+  }
 
-    // return cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
+  public static function findOrFail($slug)
+  {
+    $post = static::find($slug);
+
+    if (!$post) {
+      throw new ModelNotFoundException();
+    }
+
+    return $post;
   }
 }
