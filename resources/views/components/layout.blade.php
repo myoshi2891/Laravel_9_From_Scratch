cc0fe546d235782848bec940f72b6c93a9a6a3b4
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-13 01:03:06 +0900

    Convert the HTML and CSS to Blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,15 +2,59 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
+
+    <title>Laravel From Scratch Blog</title>
 </head>
 
-<body>
-    {{ $slot }}
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Laracast Logo" width="165" height="16" />
+                </a>
+            </div>
+            <div class="mt-8 md:mt-0">
+                <a href="/" class="test-xs font-bold uppercase">Home Page</a>
+
+                <a href="#"
+                    class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase px-5 py-3">Subscribe
+                    for Update</a>
+            </div>
+        </nav>
+
+        {{ $slot }}
+
+        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto" />
+            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+            <p class="text-sm">Promise to keep the inbox clean. No bugs.</p>
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+                    <form action="#" method="POST" class="lg:flex text-sm">
+                        <div class="lg:py-3 lg:px-5 items-center">
+                            <label for="email" class="hidden lg:inline-block">
+                                <img src="/images/mailbox-icon.svg" alt="mailbox letter" />
+                            </label>
+                            <input id="email" type="text" placeholder="Your email address"
+                                class="lg:bg-transparent pl-4 focus-within:outline-none" />
+                        </div>
+                        <button type="submit"
+                            class="bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase px-8 py-3">
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </section>
 </body>
 
 </html>
---
+++ resources/views/components/post-card.blade.php
@@ -0,0 +1,51 @@
+<article
+    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    <div class="py-6 px-5">
+        <img src="./../images/illustration-3.png" alt="Blog Post Illustration" class="rounded-xl" />
+        <div class="mt-8 flex flex-col justify-between">
+            <header>
+                <div class="space-x-2">
+                    <a href=""
+                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                        style="font-size: 10px">Techniques</a>
+                    <a href=""
+                        class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
+                        style="font-size: 10px">Updates</a>
+                </div>
+                <div class="mt-4">
+                    <h1 class="text-3xl">
+                        This is a big title and it will look great on two or even
+                        three lines. Wooohoo!
+                    </h1>
+                    <span class="mt-2 block text-gray-300 text-xs">Published
+                        <time>1 day ago</time>
+                    </span>
+                </div>
+            </header>
+            <div class="text-sm mt-4">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Iste eius accusantium exercitationem.
+                </p>
+                <p class="mt-4">
+                    Cum nostrum illum optio beatae expedita autem! Fugit fuga
+                    modi facilis quidem, enim reprehenderit incidunt eum
+                    perferendis quas.
+                </p>
+            </div>
+            <footer class="flex justify-between items-center mt-8">
+                <div class="flex items-center text-sm">
+                    <img src="./../images/lary-avatar.svg" alt="Lary avatar" />
+                    <div class="ml-3">
+                        <h5 class="font-bold">Larry Laracore</h5>
+                        <h6>Mascot at Laracast</h6>
+                    </div>
+                </div>
+                <div class="hidden lg:block">
+                    <a href="#" class="text-xs font-semibold bg-gray-200 rounded-full py-2 px-8">Read
+                        More</a>
+                </div>
+            </footer>
+        </div>
+    </div>
+</article>
