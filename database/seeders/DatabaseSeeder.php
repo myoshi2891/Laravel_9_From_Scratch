6fae6f386169b38654ab623e8b81f38e72d55fe7
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-12 16:49:53 +0900

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, placeat?',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eius excepturi repellendus tempora quae, accusantium nemo eos et, distinctio dolorum sed inventore, ipsum quis nostrum dolorem unde molestias veritatis aspernatur earum laboriosam libero labore. Dolorum eum, eos corrupti aut repudiandae ea aperiam officia asperiores non assumenda sapiente adipisci eaque.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, placeat?',
+            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eius excepturi repellendus tempora quae, accusantium nemo eos et, distinctio dolorum sed inventore, ipsum quis nostrum dolorem unde molestias veritatis aspernatur earum laboriosam libero labore. Dolorum eum, eos corrupti aut repudiandae ea aperiam officia asperiores non assumenda sapiente adipisci eaque.</p>'
+        ]);
     }
 }
