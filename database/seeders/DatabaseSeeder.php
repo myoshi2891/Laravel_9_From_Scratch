99045f544a41a42e1b6c98ea259ddac82fdbd1d6
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-12 21:48:10 +0900

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
 use App\Models\User;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,43 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, placeat?',
-            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eius excepturi repellendus tempora quae, accusantium nemo eos et, distinctio dolorum sed inventore, ipsum quis nostrum dolorem unde molestias veritatis aspernatur earum laboriosam libero labore. Dolorum eum, eos corrupti aut repudiandae ea aperiam officia asperiores non assumenda sapiente adipisci eaque.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, placeat?',
-            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam totam eius excepturi repellendus tempora quae, accusantium nemo eos et, distinctio dolorum sed inventore, ipsum quis nostrum dolorem unde molestias veritatis aspernatur earum laboriosam libero labore. Dolorum eum, eos corrupti aut repudiandae ea aperiam officia asperiores non assumenda sapiente adipisci eaque.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
