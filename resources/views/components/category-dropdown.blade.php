920b35eda55c32890fab374b0e554b2adb431fd0
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-17 23:00:44 +0900

    Merge Category and Search Queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -9,7 +9,8 @@
     <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?category={{ $category->slug }}" :active="request()->is('categories/' . $category->slug)" {{-- :active="isset($currentCategory) && $currentCategory->is($category)" --}}>
+        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            :active="request()->is('categories/' . $category->slug)">
             {{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
