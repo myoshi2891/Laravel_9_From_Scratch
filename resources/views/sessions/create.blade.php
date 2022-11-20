e091b0fee10d560fcc7237e3d2d378eeee1be1b8
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-21 01:44:30 +0900

    Build the Log In Page

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,33 +1,9 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
-            <form action="/register" method="POST" class="mt-10">
+            <h1 class="text-center font-bold text-xl">Log In!</h1>
+            <form action="/login" method="POST" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Name
-                    </label>
-
-                    <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name"
-                        value="{{ old('name') }}" required>
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Username
-                    </label>
-
-                    <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username"
-                        value="{{ old('username') }}" required>
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
                 <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         email
@@ -56,7 +32,7 @@
 
                 <div class="mb-6">
                     <button class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
+                        Log In
                     </button>
                 </div>
 
@@ -68,6 +44,7 @@
                     </ul>
                 @endif --}}
             </form>
+
         </main>
     </section>
 </x-layout>
