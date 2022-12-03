8220f38e3650d62876271339961d423b8da6066d
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-12-04 01:23:18 +0900

    Limit Access to Only Admins

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -22,5 +23,10 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
     }
 }
