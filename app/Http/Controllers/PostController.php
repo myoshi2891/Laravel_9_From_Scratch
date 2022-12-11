99f87b7ec93b34869bbc4dfde63efe8e3bb126a1
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-12-12 01:25:31 +0900

    All About Authorization

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
